3c78f7c2a56fe0be0d117d267688633d65f9d574
ogi009 <70148652+ogi009@users.noreply.github.com>
2020-11-14 13:31:48 +0100

    install rect scroll and router-dom

---
+++ src/layouts/App/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Kuba</h1>
+        </div>
+    );
+};
+
+export default App;
+
