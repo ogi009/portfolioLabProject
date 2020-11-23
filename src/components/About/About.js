dcaca7d810c6ebfd5d8abaf62c4c01de9d88c374
ogi009 <70148652+ogi009@users.noreply.github.com>
2020-11-23 14:30:28 +0100

    working on RWD

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,7 +11,10 @@ const About = () => {
                 <h1>O nas</h1>
                 <img src={decoration} alt="decoration_icon"/>
                 <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
-                <img src={signature} alt="user_signature"/>
+                <div className="signature">
+                    <img src={signature} alt="user_signature"/>
+                </div>
+
             </div>
             <div className="about_container-photo">
 
